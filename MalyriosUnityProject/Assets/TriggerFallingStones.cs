dc77b65f180281196f2cc2ed3304df6fda05821c
Louis Donath <louis.donath@online.de>
2020-03-21 09:27:39 +0100

    falling stone on boss platform enter

---
+++ MalyriosUnityProject/Assets/TriggerFallingStones.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFallingStones : MonoBehaviour
+{
+    public  GameObject fallingStone;
+    Rigidbody2D rbStone;
+    public Collider2D frictionGround;
+    private void Start() {
+        rbStone = fallingStone.GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            print("stoneShouldFall");
+            rbStone.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+            rbStone.gravityScale = 20;
+            //frictionGround.sharedMaterial.friction = 10f;
+            
+        }
+    }
+}
