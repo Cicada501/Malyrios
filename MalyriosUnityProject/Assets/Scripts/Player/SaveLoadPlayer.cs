5e8be8671cc9b13c5534034caf450907c642c1ca
Louis <louis.donath@gmail.com>
2022-08-24 01:40:46 +0200

    created cave Environment

--- MalyriosUnityProject/Assets/Scripts/Player/SaveLoadPlayer.cs
+++ MalyriosUnityProject/Assets/Scripts/Player/SaveLoadPlayer.cs
@@ -3,46 +3,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class SaveLoadPlayer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI positionText;
-
+    [SerializeField] private bool spawnAtInspectorLocation;
+    Vector3 playerInspectorLocation;
     private Player player;
     private Inventory inventory;
-    
+
 
     private void Awake()
     {
+        if (spawnAtInspectorLocation)
+        {
+            playerInspectorLocation = transform.position;
+        }
+
         LoadPlayer();
     }
+
     void Start()
     {
         LoadPlayer();
-        player = GetComponent<Player>(); 
+        player = GetComponent<Player>();
         inventory = GameObject.Find("Player").GetComponent<Inventory>();
-        
     }
 
     private void OnApplicationQuit()
     {
         SaveSystem.SavePlayer(player);
         SaveSystem.SaveInventory(inventory);
-        Debug.Log("Saved"+player.gameObject.transform.position);
+        Debug.Log("Saved" + player.gameObject.transform.position);
     }
 
 
-    public static void SavePlayer(Player player){
+    public static void SavePlayer(Player player)
+    {
         SaveSystem.SavePlayer(player);
     }
 
     //Called in Start() Should later only be called once at Application start
-    public void LoadPlayer(){
+    public void LoadPlayer()
+    {
         PlayerData data = SaveSystem.LoadPlayer();
 
         Vector3 position;
-        position.x = data.position[0];
-        position.y = data.position[1];
-        position.z = data.position[2];
+        if (spawnAtInspectorLocation)
+        {
+            position = playerInspectorLocation;
+        }
+        else
+        {
+            position.x = data.position[0];
+            position.y = data.position[1];
+            position.z = data.position[2];
+        }
+
 
         transform.position = position;
         positionText.text = position.ToString();
