6b387c01609589404a7c27d990f7041b563748ec
l-don <louis.donath@gmail.com>
2022-11-25 17:08:08 +0100

    AssetUsageDetector added
    + cleanup
    + working on fireballthrowdirection

--- MalyriosUnityProject/Assets/Scripts/Player/Abilities/FireBallProjectile.cs
+++ MalyriosUnityProject/Assets/Scripts/Player/Abilities/FireBallProjectile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,21 +9,55 @@ public class FireBallProjectile : MonoBehaviour
     [SerializeField] GameObject impactEffect = null;
     [SerializeField] private int fireballDamage;
     private Rigidbody2D rigidBody;
-    
-    
+    private Player player;
+    private bool playerFlipped;
+
+
     void Start()
     {   //rigidBody has continoous velocity forward
         rigidBody = GetComponent<Rigidbody2D>();
-        rigidBody.velocity = new Vector2(projectileSpeed, -0.05f*projectileSpeed);
+        
+        player = FindObjectOfType<Player>();
 
     }
-       
+
+    private void Update()
+    {
+        if (player.gameObject.GetComponent<Transform>().rotation.y < 0)
+        {
+            playerFlipped = true;
+        }
+        else
+        {
+            playerFlipped = false;
+        }
+        Debug.Log("player Flipped: "+playerFlipped);
+        Debug.Log(player.gameObject.GetComponent<Transform>().rotation.y);
+        
+        if (playerFlipped)
+        {
+            rigidBody.velocity = new Vector2(-projectileSpeed, -0.05f*projectileSpeed);
+            //GetComponent<SpriteRenderer>().flipX = true;
+            //impactEffect.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(projectileSpeed, -0.05f*projectileSpeed);
+        }
+
+        
+    }
+
     //On Collision of Fireball with enemy
     private void OnTriggerEnter2D(Collider2D collision){     
         if (collision.gameObject.tag == "Enemy")
         {
+           
+            //offset x value to have imact at right position
+            var position = transform.position;
+            var spawnImpactPoint = new Vector3(position.x+0.4f, position.y, position.z);
             //Show impacteffect
-            Instantiate(impactEffect,transform.position,Quaternion.identity);
+            Instantiate(impactEffect,spawnImpactPoint,Quaternion.identity);
             //Deal damage
             collision.gameObject.GetComponent<Enemy>().TakeDamage(fireballDamage);
             //Destroy fireball
