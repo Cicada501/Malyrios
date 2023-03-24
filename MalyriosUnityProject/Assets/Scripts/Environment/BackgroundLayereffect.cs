89cfbf2d3de3ea5b1f624194d90dfd2f79f53802
l-don <louis.donath@gmail.com>
2023-03-24 18:00:21 +0100

    reworked backgroundlayereffect

--- MalyriosUnityProject/Assets/Scripts/Environment/BackgroundLayereffect.cs
+++ MalyriosUnityProject/Assets/Scripts/Environment/BackgroundLayereffect.cs
@@ -1,36 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundLayereffect : MonoBehaviour
 {
-
-
-	private Transform parentObjectPositionOffset;
-	private float lengthX, startposX;
-	private float lengthY, startposY;
-	GameObject cam = null;
-	//float camPosY = 19.5f; //base value, so that the backgrounds spawn in the right position wherever the player spawns
-	[SerializeField] float parallaxEffectX = 0.0f;
-	[SerializeField] float parallaxEffectY = 0.0f;
-	
+	private float startposX;
+	private float startposY;
+	GameObject cam;
+	[SerializeField] float parallaxEffectX;
+	[SerializeField] float parallaxEffectY;
+	/*
+	 * This Background layer implements the idea of the Sprites all moving away towards the player with a certain percentage(defined by the paralax value)
+	 * of the way between player and the initial location of the object. Like this it is guaranteed to have this object always at its initial location, if the
+	 * player is at this location.
+	 * Disadvantage: The speed of the layer effect decreases when the player gets closer to the initial location
+	 */
 
 	// Use this for initialization
 	void Start ()
 	{
-		parentObjectPositionOffset = transform.parent;
 		cam = GameObject.Find("Camera");
-		startposX = transform.position.x + parentObjectPositionOffset.transform.position.x ;
-		startposY = transform.position.y + parentObjectPositionOffset.transform.position.y ;
+		startposX = transform.position.x;
+		startposY = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float distX = (cam.transform.position.x * parallaxEffectX);
-		float distY = ((cam.transform.position.y) * parallaxEffectY);
+		
+		float distX = (cam.transform.position.x - startposX) * parallaxEffectX;
+		float distY = (cam.transform.position.y - startposY) * parallaxEffectY;
 
-		transform.position = new Vector3(startposX + distX,startposY+ distY, transform.position.z);
+		transform.position = new Vector3(startposX + distX,startposY+ distY, 0);
 	}
 }
-
-//https://www.youtube.com/watch?v=zit45k6CUMk
