671873d32e25640d2106ff637af29898f3ae3e6d
l-don <louis.donath@gmail.com>
2023-08-05 11:11:39 +0200

    add Function to add new Questlog entry
    + hacked solution to solve content size fitter bug

--- MalyriosUnityProject/Assets/QuestLogWindow.cs
+++ MalyriosUnityProject/Assets/QuestLogWindow.cs
@@ -1,18 +1,94 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestLogWindow : MonoBehaviour
 {
     
     [SerializeField] private GameObject questWindow;
+    [SerializeField] private GameObject questLogEntryPrefab;
+    [SerializeField] private GameObject listOfQuests;
+    
     public List<Quest> quests = new List<Quest>();
+    
+    private int toggleCount = 0;
 
+    private void Start()
+    {
+        questWindow.SetActive(false);
+        AddQuest("Test Title", "Testing the test description, Testing the test description, Testing the test description");
+        //AddQuest("Test Title", "Testing the test description");
+        
+    }
 
     public void ToggleQuestWindow()
     {
         questWindow.SetActive(!questWindow.activeSelf);
+        
+    }
+
+
+
+    public void AddQuest(string title, string description)
+    {
+        GameObject newQuest = Instantiate(questLogEntryPrefab, listOfQuests.transform);
+        
+        TextMeshProUGUI titleText = newQuest.transform.Find("title+marker/title").GetComponent<TextMeshProUGUI>();
+        titleText.text = title;
+
+        TextMeshProUGUI descriptionText = newQuest.transform.Find("desc+offsetimg/description").GetComponent<TextMeshProUGUI>();
+        descriptionText.text = description;
+
+        Quest newQuestObj = new Quest { questName = title, questDescription = description };
+        quests.Add(newQuestObj);
+        SetTransparency(questWindow.transform,0);
+        StartCoroutine(ActivateChildren());
+        
+
+    }
+        private IEnumerator ActivateChildren()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                questWindow.SetActive(!questWindow.activeSelf);
+                yield return new WaitForSeconds(.1f);
+                questWindow.SetActive(!questWindow.activeSelf);
+            }
+            
+            SetTransparency(questWindow.transform,1);
+        }
+
+
+
+    public void UpdateQuest(string title, string description)
+    {
+        
+    }
+
+    public void RemoveQuest(string title, string description)
+    {
+        
+    }
+    
+    private void SetTransparency(Transform parent, float transparency)
+    {
+        var graphic = parent.GetComponent<Graphic>();
+        if (graphic != null)
+        {
+            var color = graphic.color;
+            color.a = transparency;
+            graphic.color = color;
+        }
+        if (parent.childCount > 0)
+        {
+            foreach (Transform child in parent)
+            {
+                SetTransparency(child, transparency);
+            }
+        }
     }
 }
 
