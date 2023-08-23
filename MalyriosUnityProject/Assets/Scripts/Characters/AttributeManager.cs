08759965c19d0d5911f87b977171e6b8698d944b
l-don <louis.donath@gmail.com>
2023-08-23 18:02:36 +0200

    add Armor logic to attribute manager

--- MalyriosUnityProject/Assets/Scripts/Characters/AttributeManager.cs
+++ MalyriosUnityProject/Assets/Scripts/Characters/AttributeManager.cs
@@ -17,13 +17,76 @@ public class AttributeManager : MonoBehaviour
     private ISlot bodyArmorItem;
     private ISlot handArmorItem;
     private ISlot feetArmorItem;
-   
+    private ISlot weaponItem;
+    
     private BaseWeapon weapon;
+    private BaseArmor headArmor;
+    private BaseArmor bodyArmor;
+    private BaseArmor handArmor;
+    private BaseArmor feetArmor;
     
     private void Awake()
     {
         this.baseAttributes = ReferencesManager.Instance.player.GetComponent<BaseAttributes>();
+        
+        weaponItem = weaponSlot.GetComponent<ISlot>();
+        headArmorItem = headArmorSlot.GetComponent<ISlot>();
+        bodyArmorItem = bodyArmorSlot.GetComponent<ISlot>();
+        handArmorItem = handArmorSlot.GetComponent<ISlot>();
+        feetArmorItem = feetArmorSlot.GetComponent<ISlot>();
+        
+        EquipmentSlot.OnArmorChanged += OnArmorChanged;
         EquipmentSlot.OnWeaponChanged += OnWeaponChanged;
+    }
+    
+    private void OnArmorChanged(BaseArmor newArmor)
+    {
+        if (newArmor.ItemType == BaseItem.ItemTypes.Head)
+        {
+            OnArmorSwap(ref headArmor, newArmor);
+        }
+        else if (newArmor.ItemType == BaseItem.ItemTypes.Body)
+        {
+            OnArmorSwap(ref bodyArmor, newArmor);
+        }
+        else if (newArmor.ItemType == BaseItem.ItemTypes.Hand)
+        {
+            OnArmorSwap(ref handArmor, newArmor);
+        }
+        else if (newArmor.ItemType == BaseItem.ItemTypes.Feet)
+        {
+            OnArmorSwap(ref feetArmor, newArmor);
+        }
+    }
+    
+    private void OnArmorSwap(ref BaseArmor currentArmor, BaseArmor newArmor)
+    {
+        if (currentArmor)
+        {
+            OnArmorRemove(currentArmor);
+        }
+
+        OnArmorAdd(newArmor);
+        currentArmor = newArmor;
+    }
+
+    private void OnArmorAdd(BaseArmor armor)
+    {
+        if (!armor) return;
+        this.baseAttributes.MaxHealth += armor.HealthBonus;
+        this.baseAttributes.Strength += armor.StrengthBonus;
+        this.baseAttributes.CritChance += armor.CritChanceBonus;
+        this.baseAttributes.CritDamage += armor.CritDamageBonus;
+        //...
+    }
+
+    private void OnArmorRemove(BaseArmor armor)
+    {
+        if (!armor) return;
+        this.baseAttributes.MaxHealth -= armor.HealthBonus;
+        this.baseAttributes.Strength -= armor.StrengthBonus;
+        this.baseAttributes.CritChance -= armor.CritChanceBonus;
+        this.baseAttributes.CritDamage -= armor.CritDamageBonus;
     }
 
     private void OnWeaponChanged(BaseWeapon wpn)
