382fd8462181cfe8622071488a4de71851c534c8
l-don <louis.donath@gmail.com>
2023-09-06 09:44:07 +0200

    destroy spear after 5 seconds

--- MalyriosUnityProject/Assets/Projectile.cs
+++ MalyriosUnityProject/Assets/Projectile.cs
@@ -4,18 +4,28 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private int damage = 1;
+    [SerializeField] private float destroyedAfterSeconds;
     private Rigidbody2D rb;
     [HideInInspector]
     public Enemy parentEnemy;
     private Vector2 initialVelocityDirection;
+    private float lifeTime;
 
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity =  (parentEnemy.facingRight ? Vector2.right : Vector2.left)*speed;
         transform.parent = null;
     }
 
+    private void Update()
+    {
+        lifeTime += Time.deltaTime;
+        if (lifeTime > destroyedAfterSeconds)
+        {
+            Destroy(gameObject);  
+        }
+    }
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
         
